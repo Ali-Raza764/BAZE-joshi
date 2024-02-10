0a2a387a4be7521dd7f025c24dbdcaa046a9fce2
uncle-joshi <zainulabdin1320@gmial.com>
2024-02-10 05:24:41 -0800

    changes

--- src/Components/ImgSlider.jsx
+++ src/Components/ImgSlider.jsx
@@ -47,11 +47,9 @@ const ImgSlider = () => {
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
       ></div>
-      {/* Left Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </div>
-      {/* Right Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
@@ -62,7 +60,7 @@ const ImgSlider = () => {
             onClick={() => goToSlide(slideIndex)}
             className='text-2xl cursor-pointer'
           >
-            <RxDotFilled />
+            <RxDotFilled className='text-white'/>
           </div>
         ))}
       </div>
